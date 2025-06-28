3f74ae3a2e46ae5e08c5239762ec97e90b26fbd6
Jordan Needham <jdncreations@hotmail.com>
2025-06-28 21:40:01 +1200

    add cors

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import { setupAssociations } from './models/Associations.js';
 import seedDB from './seed.js';
 import categoryRouter from './routes/categoryRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV || 'dev';
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/categories', categoryRouter);
 app.use('/api/products', productRouter);
